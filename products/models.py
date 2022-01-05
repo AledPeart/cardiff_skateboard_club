b8e36999c6bca45393da38d5c76ed1bdc91a1d37
AledPeart <aledpeart@gmail.com>
2022-01-05 19:32:06 +0000

    Ammended product_code field in products model

--- products/models.py
+++ products/models.py
@@ -39,7 +39,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    product_code = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    product_code = models.CharField(max_length=254, null=True, blank=True)
     image_src = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
