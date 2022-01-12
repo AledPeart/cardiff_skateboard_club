1691aa9890346548ae14b78a9c21c3a4f855acbd
AledPeart <aledpeart@gmail.com>
2022-01-12 14:53:00 +0000

    Added Stripe Elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51K17HVK5RGaoYR0pmorsKsaG9fzfoqm5y6wLAh6UNoWZXrUyloMFphj7faMMc9aDeFcqxy3NXBRxMdPfUxbBDzVy00UZ6OXASy',
+        'clent_secret': 'test client secret',
     }
 
     return render(request, template, context)
