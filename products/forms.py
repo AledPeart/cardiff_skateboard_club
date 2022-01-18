426f487fbb19b49e0e0096e1069f7cd060ac1600
AledPeart <aledpeart@gmail.com>
2022-01-18 11:24:22 +0000

    Added add_product template/view

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, Category
+from .models import Product, Category, Brand
 
 
 class ProductForm(forms.ModelForm):
@@ -15,7 +15,7 @@ class ProductForm(forms.ModelForm):
         category_friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
         brand_friendly_names = [(b.id, b.get_friendly_name()) for b in brands]
 
-        self.fields['category'].choices = friendly_names
+        self.fields['category'].choices = category_friendly_names
         self.fields['brand'].choices = brand_friendly_names
 
         for field_name, field in self.fields.items():
