b7c4e3f436bfdb483a5f24c57f63f7c85a855e5e
AledPeart <aledpeart@gmail.com>
2022-02-01 20:23:06 +0000

    Ammended layout/styling of Navbar

--- cardiff_skateboard_club/settings.py
+++ cardiff_skateboard_club/settings.py
@@ -122,17 +122,17 @@ WSGI_APPLICATION = 'cardiff_skateboard_club.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if 'DATABASE_URL' in os.environ:  
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
